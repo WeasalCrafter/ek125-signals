e60f7a6d12d5334eced900c251c6225bd5a09018
Logan Fick <loganfick07@gmail.com>
2025-11-17 15:56:15 -0500

    instructions for accessing git

--- logan.m
+++ logan.m
@@ -1,1 +1,20 @@
 % logans code
+% https://github.com/WeasalCrafter/ek125-signals.git
+
+
+% run matlab.git.clearCredential("https://github.com/WeasalCrafter/ek125-signals.git")
+
+% 1) username: create an account on github and remember the username
+
+% 2) token: 
+%    click profile in github
+%    -> settings
+%        scroll to the bottom and select developer settings
+%        -> select personal access token on the left
+%            -> Tokens(Classic)
+%        name it whatever,select just "repo" for the scopes, then generate token,
+%        make sure to copy it and save it somewhere, it will not be shown
+%        again
+
+%   try to push in matlab and it will prompt you for username and token
+%   use your own username and the token you copied to sign in
